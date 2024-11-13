d3d9ebf7eeb2acc77e478447813afc586df03dac
Ashbipbinu <abinu@thoughtlinetech.com>
2024-11-13 12:17:26 +0530

    server updated with dotenv and mongodb configuration

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = express();
-
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Successfully connected to the database")
+}).catch(err => {
+    console.log("Failed to connect with database", err)
+})
 
 app.listen(3000, () => {
     console.log("Server listening to port 3000");
